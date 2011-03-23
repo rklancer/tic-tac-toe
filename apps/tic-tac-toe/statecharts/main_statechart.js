41909216739aa7a773e29c5a94f379a39f9db419
Richard Klancer <rpk@pobox.com>
2011-03-23 13:58:46 -0400

    Comment out statechart for now.
    Now TemplatePane appends & specs run, but fail.

--- apps/tic-tac-toe/statecharts/main_statechart.js
+++ apps/tic-tac-toe/statecharts/main_statechart.js
@@ -1,41 +1,41 @@
-TicTacToe.mainStatechart = Ki.Statechart.create({
-  initialState: 'startGame',
-
-  startGame: Ki.State.design({
-    enterState: function() {
-      TicTacToe.currentGame = TicTacToe.Game.createNewGame();
-      TicTacToe.gameController.set('content', TicTacToe.currentGame);
-      TicTacToe.mainPage.get('mainPane').append();
-      this.gotoState('player1sTurn');
-    }
-  }),
-
-  player1sTurn: Ki.State.design({
-    enterState: function() {
-      TicTacToe.currentGame.set('currentPlayer', 1);
-    },
-
-    markCell: function() {
-      var currentCell = TicTacToe.currentCellController.get('content');
-      if(TicTacToe.currentGame.markCell(currentCell, 1)) this.gotoState('player2sTurn');
-    }
-  }),
-
-  player2sTurn: Ki.State.design({
-    enterState: function() {
-      TicTacToe.currentGame.set('currentPlayer', 2);
-    },
-
-    markCell: function() {
-      var currentCell = TicTacToe.currentCellController.get('content');
-      if(TicTacToe.currentGame.markCell(currentCell, 2)) this.gotoState('player1sTurn');
-    }
-  }),
-
-  endGame: Ki.State.design({
-    enterState: function() {
-      TicTacToe.currentGame.destroy();
-      TicTacToe.mainPage.get('mainPane').remove();
-    }
-  })
-});
+// TicTacToe.mainStatechart = Ki.Statechart.create({
+//   initialState: 'startGame',
+// 
+//   startGame: Ki.State.design({
+//     enterState: function() {
+//       TicTacToe.currentGame = TicTacToe.Game.createNewGame();
+//       TicTacToe.gameController.set('content', TicTacToe.currentGame);
+//       TicTacToe.mainPage.get('mainPane').append();
+//       this.gotoState('player1sTurn');
+//     }
+//   }),
+// 
+//   player1sTurn: Ki.State.design({
+//     enterState: function() {
+//       TicTacToe.currentGame.set('currentPlayer', 1);
+//     },
+// 
+//     markCell: function() {
+//       var currentCell = TicTacToe.currentCellController.get('content');
+//       if(TicTacToe.currentGame.markCell(currentCell, 1)) this.gotoState('player2sTurn');
+//     }
+//   }),
+// 
+//   player2sTurn: Ki.State.design({
+//     enterState: function() {
+//       TicTacToe.currentGame.set('currentPlayer', 2);
+//     },
+// 
+//     markCell: function() {
+//       var currentCell = TicTacToe.currentCellController.get('content');
+//       if(TicTacToe.currentGame.markCell(currentCell, 2)) this.gotoState('player1sTurn');
+//     }
+//   }),
+// 
+//   endGame: Ki.State.design({
+//     enterState: function() {
+//       TicTacToe.currentGame.destroy();
+//       TicTacToe.mainPage.get('mainPane').remove();
+//     }
+//   })
+// });
