0798aa39df386172d60816bd48978530b9d73012
Richard Klancer <rpk@pobox.com>
2011-03-23 18:12:14 -0400

    Generate HTML (can't see it because of styles)

--- apps/tic-tac-toe/tic-tac-toe.js
+++ apps/tic-tac-toe/tic-tac-toe.js
@@ -23,7 +23,10 @@ TicTacToe.appendMainPane = function () {
   });
 };
 
-TicTacToe.Cell = SC.Object.extend();
+TicTacToe.Cell = SC.Object.extend({
+  row: null,
+  column: null
+});
 
 TicTacToe.Game = SC.Object.extend({
   cells: [],
@@ -40,14 +43,69 @@ TicTacToe.Game = SC.Object.extend({
 TicTacToe.Game.createNewGame = function () {
   var game = TicTacToe.Game.create(),
       cells = game.set('cells', []).get('cells'),
-      i;
-  
-  for (i = 1; i <= 9; i++) {
-    cells.pushObject(TicTacToe.Cell.create());
-  }
+      row, col;
+
+    for (row = 1; row <= 3; row++) {
+      for (col = 1; col <= 3; col++) {
+        cells.pushObject(TicTacToe.Cell.create({row: row, column: col}));
+      }
+    }
+
   return game;
 };
+
 
 TicTacToe.gameController = SC.Object.create();
 
 TicTacToe.currentCellController = SC.Object.create();
+
+
+TicTacToe.gameView = SC.TemplateView.create({
+  layerId: 'ticTacToeGame',
+  whoseTurnIsItBinding: 'TicTacToe.currentGame.currentPlayer'
+});
+
+
+TicTacToe.gameBoardView = SC.TemplateCollectionView.create({
+  layerId: 'gameBoard',
+  contentBinding: 'TicTacToe*currentGame.cells'
+});
+
+
+TicTacToe.CellView = SC.TemplateView.extend({
+  columnBinding: '.parentView.content.column',
+  rowBinding: '.parentView.content.row',
+  belongsToPlayerBinding: '.parentView.content.belongsToPlayer',
+  
+  positionalClasses: function () {
+    var columns = ['undef', 'leftColumn', 'centerColumn', 'rightColumn'],
+        rows    = ['undef', 'topRow',     'middleRow',    'bottomRow'  ],
+        
+        col     = this.getPath('parentView.content.column') || 0,
+        row     = this.getPath('parentView.content.row')    || 0;
+        
+    return columns[col] + ' ' + rows[row];
+  }.property('row', 'column'),
+  
+  top: function () {
+    return (this.getPath('parentView.content.row') - 1) * 100;
+  }.property('row'),
+  
+  left: function () {
+    return (this.getPath('parentView.content.column') -1 ) * 100;
+  }.property('column'),
+  
+  mark: function () {
+    var markFor = ["", "X", "O"],
+        player  = this.get('belongsToPlayer') || 0 ;
+
+    return markFor[player];
+  }.property('belongsToPlayer'),
+  
+  playerClass: function () {
+    var classFor = ['', 'player1', 'player2'],
+        player  = this.get('belongsToPlayer') || 0 ;
+        
+    return classFor[player];
+  }
+});
