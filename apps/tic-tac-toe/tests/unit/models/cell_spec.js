cb2ddc7772a5610cb07cb69306259882c46568d6
Richard Klancer <rpk@pobox.com>
2011-03-23 14:35:03 -0400

    Failing spec for TicTacToe.Cell
    test url: /static/tic-tac-toe/en/current/tests/unit/models/cell_spec.html

--- apps/tic-tac-toe/tests/unit/models/cell_spec.js
+++ apps/tic-tac-toe/tests/unit/models/cell_spec.js
@@ -1,4 +1,8 @@
 /*globals TicTacToe describe it expect xit xdescribe beforeEach afterEach spyOn runs waits waitsFor clickOn fillIn */
 
-describe("TicTacToe.Cell", function() {
+describe("TicTacToe.Cell", function () {
+  it("should be an SC class", function () {
+    expect(SC.kindOf(TicTacToe.Cell, SC.Object)).toBe(true);
+    expect(TicTacToe.Cell.isClass).toBe(true);
+  });
 });
